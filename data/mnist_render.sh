fb9aac1fd5970144b3d62b7cfbd5ff424e5fe7a1
oliv <oliv@oliv.xxx>
2017-02-03 19:39:05 -0800

    clean py file at the end

--- data/mnist_render.sh
+++ data/mnist_render.sh
@@ -16,3 +16,4 @@ mkdir -p $MNIST_RENDER_DIR
 $WGET -P $MNIST_RENDER_DIR https://raw.githubusercontent.com/oliviersoares/mnist/master/mnist_render.py
 $PYTHON $MNIST_RENDER_DIR/mnist_render.py -out $MNIST_RENDER_DIR -num 60000 -seed 101 -dmax 1.0 -dataset -prefix train
 $PYTHON $MNIST_RENDER_DIR/mnist_render.py -out $MNIST_RENDER_DIR -num 10000 -seed 102 -dmax 1.0 -dataset -prefix t10k
+rm -f $MNIST_RENDER_DIR/mnist_render.py
